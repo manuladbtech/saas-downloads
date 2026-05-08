1bd58f5627f8a5e4d6a2605f271ae9c7d7e106f3
Manula Perera <manulaperera1232@gmail.com>
2026-05-08 16:39:17 +0530

    SAAS_0.0.32_arm64 and SAAS_0.0.33

--- apk_download_test/architecture-detection.js
+++ apk_download_test/architecture-detection.js
@@ -22,8 +22,8 @@ function setDownloadLinks() {
     // if (btn32)   btn32.href   = `${BASE_URL}/SAAS_${APP_VERSION}_arm.apk`;
     // if (btnUniv) btnUniv.href = `${BASE_URL}/SAAS_${APP_VERSION}_universal.apk`;
 
-    if (btn64) btn64.href = `${BASE_URL}/SAAS_0.0.28_arm64.apk`;
-    if (btn32) btn32.href = `${BASE_URL}/SAAS_0.0.29_arm.apk`;
+    if (btn64) btn64.href = `${BASE_URL}/SAAS_0.0.32_arm64.apk`;
+    if (btn32) btn32.href = `${BASE_URL}/SAAS_0.0.33_arm.apk`;
     // if (btnUniv) btnUniv.href = `${BASE_URL}/SAAS_0.0.27_universal.apk`;
 }
 
